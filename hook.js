edb9ce9aaa3c7973f4ff4773eb27a60fa90b6e7d
unscriptable <john@unscriptable.com>
2014-03-14 23:47:48 -0400

    First commit.

--- hook.js
+++ hook.js
@@ -1,3 +1,14 @@
 /** @license MIT License (c) copyright 2014 original authors */
 /** @author Brian Cavalier */
 /** @author John Hann */
+
+module.exports = function (zipManager) {
+	// create an ES6 Loader fetch hook
+	return function (load) {
+		// find the zip archive and fetch it if it's not local
+		return zipManager.forUrl(load.address, true)
+			.then(function (zip) {
+				return zip.read(load.address);
+			});
+	};
+};
